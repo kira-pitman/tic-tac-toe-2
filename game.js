5ad3b241ca16574670b11908cf1bcd5dddb5cd77
Kira Pitman <kirapitman@gmail.com>
2024-03-06 15:45:19 +1300

    complete logic for game to work

--- game.js
+++ game.js
@@ -8,14 +8,93 @@ console.log('Welcome to Tic-Tac-Toe! Enjoy!')
 // logic to display "_ WON!" when a player wins.
 
 let cells = document.getElementsByTagName('TD')
-let noughtsTurn = true // start with noughts
-let gameisover // game will be over when winning conditions met
-
-function cellClicked(e) {
-  let cell = e.target
-  console.log('clicked')
-}
 
 for (let i = 0; i < cells.length; i++) {
   cells[i].onclick = cellClicked
+}
+
+let noughtsTurn = true // start with noughts
+let gameIsOver // game will be over when winning conditions met
+
+function checkForWin(symbol) {
+  // HORIZONTAL LINES //
+
+  if (
+    cells[0].innerHTML == symbol &&
+    cells[1].innerHTML == symbol &&
+    cells[2].innerHTML == symbol
+  )
+    gameIsOver = true
+  else if (
+    cells[3].innerHTML == symbol &&
+    cells[4].innerHTML == symbol &&
+    cells[5].innerHTML == symbol
+  )
+    gameIsOver = true
+  else if (
+    cells[6].innerHTML == symbol &&
+    cells[7].innerHTML == symbol &&
+    cells[8].innerHTML == symbol
+  )
+    gameIsOver = true
+
+  // VERTICAL LINES //
+
+  if (
+    cells[0].innerHTML == symbol &&
+    cells[3].innerHTML == symbol &&
+    cells[6].innerHTML == symbol
+  )
+    gameIsOver = true
+  else if (
+    cells[1].innerHTML == symbol &&
+    cells[4].innerHTML == symbol &&
+    cells[7].innerHTML == symbol
+  )
+    gameIsOver = true
+  else if (
+    cells[2].innerHTML == symbol &&
+    cells[5].innerHTML == symbol &&
+    cells[8].innerHTML == symbol
+  )
+    gameIsOver = true
+
+  // DIAGONAL LINES //
+
+  if (
+    cells[0].innerHTML == symbol &&
+    cells[4].innerHTML == symbol &&
+    cells[8].innerHTML == symbol
+  )
+    gameIsOver = true
+  else if (
+    cells[2].innerHTML == symbol &&
+    cells[4].innerHTML == symbol &&
+    cells[6].innerHTML == symbol
+  )
+    gameIsOver = true
+
+  if (gameIsOver) {
+    subtitle.innerHTML = `Congratulations ${symbol}, you won the game!`
+  }
+}
+
+function cellClicked(e) {
+  let cells = e.target
+
+  if (cells.innerHTML == '' && noughtsTurn) {
+    cells.innerHTML = 'O'
+  }
+  checkForWin('O')
+
+  if (!gameIsOver) {
+    noughtsTurn = !noughtsTurn
+    subtitle.innerHTML = 'Your turn X'
+  }
+
+  if (cells.innerHTML == '') {
+    cells.innerHTML = 'X'
+    subtitle.innerHTML = 'Your turn O'
+    checkForWin('X')
+  }
 }
